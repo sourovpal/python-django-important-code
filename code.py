e17ed72449187bbb7a7ee57d7dd894e17d747aa2
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-09-20 06:25:20 +0600

    Create code.py

---
+++ code.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
