71d36a45c99eb85cd715740a35cc4f453b4f896f
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2024-02-09 08:46:29 +0600

    Create query-object.py

---
+++ query-object.py
@@ -0,0 +1,34 @@
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name  = models.CharField(max_length=30)
+
+    @property
+    def full_name(self):
+        "Returns the person's full name."
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta():
+        db_table = "persons"
+
+
+class Result(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    cgpa = models.FloatField()
+    SEMESTER_LIST = {
+        "1":"First Semester",
+        "2":"Second Semester",
+        "3":"Third Semester",
+    }
+    semester = models.CharField(max_length=255, choices=SEMESTER_LIST)
+
+    class Meta():
+        db_table = "results"
+
+
+persons = Person.objects.filter(first_name="Sourov",last_name="Sourov")
+    # Result.objects.create(person=persons, cgpa="3.85", semester="1")
+    for person in persons:
+        result = person.result_set.first()
+        if result:
+            print(result.cgpa)
